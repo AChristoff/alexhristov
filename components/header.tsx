e6b361641e8a956326eba7008398896cd9dd3664
Alex <alex@childish.eu>
2023-08-02 00:59:17 +0300

    add mobile nav

--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,18 @@
-import { siteConfig } from "@/config/site"
-import { MainNav } from "@/components/main-nav"
-import { ThemeToggle } from "@/components/theme-toggle"
-import { Lang } from "@/types"
+import { Lang } from '@/types'
 
-import LanguageSwitcher from "./language-switcher"
+import { siteConfig } from '@/config/site'
+import { MainNav } from '@/components/main-nav'
+import { ThemeToggle } from '@/components/theme-toggle'
 
-export function Header({lng}: {lng: Lang}) {
+import LanguageSwitcher from './language-switcher'
+
+export function Header({ lng }: { lng: Lang }) {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background drop-shadow-sm">
       <section className="md:container md:mx-auto flex flex-1 items-center space-x-4 h-16 px-1">
         <MainNav items={siteConfig.mainNav} lng={lng} />
-        <section className="flex items-center space-x-1">
-          <span className="border-l h-5 mr-2"/>
+        <section className="hidden md:flex items-center space-x-1">
+          <span className="border-l h-5 mr-2" />
           <ThemeToggle />
           <LanguageSwitcher />
         </section>
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,10 +3,15 @@
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Lang } from '@/types'
+import { MenuIcon, X } from 'lucide-react'
 
 import { NavItem } from '@/types/nav'
 import { Icons } from '@/config/icons'
 import { cn } from '@/lib/utils'
+
+import LanguageSwitcher from './language-switcher'
+import { ThemeToggle } from './theme-toggle'
+import { Button } from './ui/button'
 
 interface MainNavProps {
   items?: NavItem[]
@@ -14,11 +19,12 @@ interface MainNavProps {
 }
 
 export function MainNav({ items, lng }: MainNavProps) {
-  const [activeLand, setActiveLand] = useState('')
+  const [activeLang, setActiveLang] = useState('')
+  const [mobileNavOpen, setMobileNavOpen] = useState(false)
 
   useEffect(() => {
     const lang = window.location.pathname.split('/')[1]
-    setActiveLand(lang)
+    setActiveLang(lang)
   }, [])
 
   return (
@@ -26,14 +32,20 @@ export function MainNav({ items, lng }: MainNavProps) {
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-9" />
       </Link>
+      <Button
+        className="px-3 md:hidden"
+        onClick={() => setMobileNavOpen((state) => !state)}
+      >
+        {mobileNavOpen ? <X className="h-7" /> : <MenuIcon className="h-7" />}
+      </Button>
       {items?.length ? (
-        <nav className="flex gap-6 w-auto">
+        <nav className="hidden md:flex gap-6 w-auto">
           {items?.map(
             (item, index) =>
               item.href && (
                 <Link
                   key={index}
-                  href={`/${activeLand}${item.href}`}
+                  href={`/${activeLang}${item.href}`}
                   className={cn(
                     'uppercase flex items-center text-sm font-medium text-muted-foreground hover:text-primary',
                     item.disabled && 'cursor-not-allowed opacity-80'
@@ -45,6 +57,34 @@ export function MainNav({ items, lng }: MainNavProps) {
           )}
         </nav>
       ) : null}
+      {mobileNavOpen && (
+        <section className="md:hidden absolute bg-background w-full pt-5 px-3 mt-12 drop-shadow-lg border-t">
+          <nav className="flex flex-col gap-6 w-auto">
+            {items?.map(
+              (item, index) =>
+                item.href && (
+                  <Link
+                    key={index}
+                    href={`/${activeLang}${item.href}`}
+                    className={cn(
+                      'uppercase flex items-center text-sm font-medium text-muted-foreground hover:text-primary',
+                      item.disabled && 'cursor-not-allowed opacity-80'
+                    )}
+                  >
+                    {item.title[lng]}
+                  </Link>
+                )
+            )}
+          </nav>
+          <section className="flex flex-col py-2">
+            <span className="border-t w-20 my-2" />
+            <span>
+              <ThemeToggle />
+              <LanguageSwitcher />
+            </span>
+          </section>
+        </section>
+      )}
     </div>
   )
 }
