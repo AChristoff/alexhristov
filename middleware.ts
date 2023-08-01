096758daf7fdc006aad3468c77c498758a4345c7
Alex <alex@childish.eu>
2023-08-01 14:44:17 +0300

    fix middleware and add robots.txt

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,42 @@
-import { NextResponse } from 'next/server'
-import acceptLanguage from 'accept-language'
-import { fallbackLng, languages } from './app/i18n/settings'
+import { NextRequest, NextResponse } from "next/server"
+import acceptLanguage from "accept-language"
+
+import { fallbackLng, languages } from "./app/i18n/settings"
 
 acceptLanguage.languages(languages)
 
 export const config = {
-  // matcher: '/:lng*'
-  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.svg|sw.js).*)']
-}
+	// Matcher ignoring `/_next/`, `/api/` '/images' and '/public' subpaths
+	matcher: [
+		'/((?!api|_next/static|_next/image|robots.txt|public|images|manifest.json|sw.js|favicon.svg|workbox-*).*)',
+		'/'
+	],
+};
 
-const cookieName = 'i18next'
+const cookieName = "i18next"
 
-export function middleware(req: any) {
+export function middleware(req: NextRequest) {
   let lng
-  if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value)
-  if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
+  if (req.cookies.has(cookieName))
+    lng = acceptLanguage.get(req.cookies.get(cookieName)?.value)
+  if (!lng) lng = acceptLanguage.get(req.headers.get("Accept-Language"))
   if (!lng) lng = fallbackLng
 
   // Redirect if lng in path is not supported
   if (
-    !languages.some(loc => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
-    !req.nextUrl.pathname.startsWith('/_next')
+    !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
+    !req.nextUrl.pathname.startsWith("/_next")
   ) {
-    return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url))
+    return NextResponse.redirect(
+      new URL(`/${lng}${req.nextUrl.pathname}`, req.url)
+    )
   }
 
-  if (req.headers.has('referer')) {
-    const refererUrl = new URL(req.headers.get('referer'))
-    const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`))
+  if (req.headers.has("referer")) {
+    const refererUrl = new URL(req.headers.get("referer") as string)
+    const lngInReferer = languages.find((l) =>
+      refererUrl.pathname.startsWith(`/${l}`)
+    )
     const response = NextResponse.next()
     if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
     return response
