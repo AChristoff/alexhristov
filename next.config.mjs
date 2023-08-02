f104354564a4034f1ee560c9dc15b54b744d43fc
Alex <alex@childish.eu>
2023-08-02 10:33:31 +0300

    update config to be empty

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-/**
- * @type {import('next').NextConfig}
-*/
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
 module.exports = nextConfig
