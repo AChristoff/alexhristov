42ef07a588fbec84f0811069a7dc5d8ac7ed4d40
Alex <alex@childish.eu>
2023-08-02 10:55:33 +0300

    new conf update

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  experimental: {
-    appDir: true,
-    images: {
-      unoptimized: true,
-    },
-  },
 }
 
 export default nextConfig
