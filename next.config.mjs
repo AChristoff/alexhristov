ec4b590045ccb5f75f021f4dda7b0c8c71d9c484
Alex <alex@childish.eu>
2023-08-02 10:46:45 +0300

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
-/**
- * @type {import('next').NextConfig}
-*/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'export',
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  experimental: {
+    appDir: true,
+  },
 }
- 
-module.exports = nextConfig
+
+export default nextConfig
