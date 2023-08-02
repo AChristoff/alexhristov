c9d3e147b50200bd36382ad44eba088ccbd4b61b
Alex <alex@childish.eu>
2023-08-02 16:28:33 +0300

    add translations to not found page

--- app/[lng]/not-found.tsx
+++ app/[lng]/not-found.tsx
@@ -4,21 +4,24 @@ import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
 
+import { useTranslation } from '@/i18n'
+
 type Props = {
   params: {
     lng: string
   }
 }
 const NotFound = async ({ params }: Props) => {
+  const { t } = await useTranslation(params?.lng, 'home')
 
   return (
     <section className="container text-center">
       <h1 className="text-center uppercase font-semibold leading-tight text-xl tracking-[3px] my-5">
-        Page not found
+        {t('notFound')}
       </h1>
       <Button className="my-2">
-        <Link className="text-center" href={`/${params?.lng}/`}>
-          {params?.lng === 'bg' ? 'Към начало' : 'Go to home'}
+        <Link className="text-center" href={`/${params?.lng}`}>
+          {t('goHome')}
         </Link>
       </Button>
       <Image
@@ -28,6 +31,7 @@ const NotFound = async ({ params }: Props) => {
         height={0}
         sizes="100vw"
         className="mx-auto"
+        placeholder="blur"
       />
     </section>
   )
