b8b6ab318ac03914baaf385ec1586fec9710c244
Alex <alex@childish.eu>
2023-08-02 16:00:50 +0300

    prepare for vercel

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
  * @type {import('next').NextConfig}
 */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
+  // reactStrictMode: true,
+  // output: 'export',
 }
  
 module.exports = nextConfig
