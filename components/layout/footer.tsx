8d9142a01dee9d95120b77cd27d07579eb488d53
Alex <alex@childish.eu>
2023-08-14 09:31:54 +0300

    enlarge mobile nav toggle menu button

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="border-t bg-background/80 backdrop-blur-sm" style={{backdropFilter: 'blur(10px)'}}>
+    <footer className="border-t bg-background/80 backdrop-blur-sm">
       <section className="container flex items-center justify-center py-1">
         <p className="text-sm font-medium  mr-3 text-muted-foreground">&copy;{` 2019-${currentYear}`}</p>
         <Link
